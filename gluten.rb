f267747e98bfbc4cafc239f1baa339bf317ccb93
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-01-10 00:50:22 -0800

    made some changes

--- gluten.rb
+++ gluten.rb
@@ -7,41 +7,46 @@ food_order = {"pizza" => "cheese, gluten, tomatoes",
 
 class Person 
 
-	attr_accessor :food_order, :stom, :allergies 
-	def initialize (stom = [], allergies)
-		@stom = stom
-		@allergies = allergies
-		@food = food_order
-	end
+        attr_accessor :food_order, :stom, :allergies 
+        def initialize (stom = [], allergies)
+                @stom = stom
+                @allergies = allergies
+                @food = food_order
+        end
 
-	def eat 
-		@food = @stom
-		food_order = @food
-		food_consumed = food_order.values.join
-		# @stomach << foods
-		# puts "This is #{food_consumed}"
-		# p "#{food_consumed}"
-		#puts "This is his allergy: #{@allergies}"
-		begin
-		if food_consumed.include?(@allergies)
-		raise AllergyError.new("ALLERGYERROR: Oh No! You just ate #{@allergies}")
-		else
-			puts 'You had a good meal!'
-		end
-		rescue AllergyError => e
-		puts "#{e}"
-		end
-	end
+        def puke
+        	@stom.clear
+        	puts "Bleeehhhhhh!!"
+        end
+
+        def eat 
+                begin
+                @stom.each do |key, value|
+                        food_consumed = value
+                        if food_consumed.include?(@allergies)
+                        		self.puke
+                                raise AllergyError.new("ALLERGYERROR: Oh No! You just ate #{@allergies}")    
+                        end
+                end
+                puts 'Have a good meal!'
+                rescue AllergyError => e
+                        puts "#{e}"
+                end
+        end
 end
 
 class AllergyError < NoMethodError; end
 
-	
+        
 # end
 
-begin
 chris = Person.new(food_order, "gluten")
+puts "CHRIS:"
 chris.eat
-rescue AllergyError => e
-	puts "#{e}"
-end
+puts " "
+puts "BETH:"
+beth = Person.new(food_order, "o")
+beth.eat
+#rescue AllergyError => e
+        #puts "#{e}"
+#end
